e7ea7b34fe8648d7239d45ba3fd513bc3b5ca760
yanismerzkane <105671997+yanismerzkane@users.noreply.github.com>
2022-05-21 09:42:09 +0100

    Update twitter reply bot.py

--- twitter reply bot.py
+++ twitter reply bot.py
@@ -19,20 +19,20 @@ message = "si vous avez des questions , n'hesiter pas a les envoyer "
 
 # The actual bot
 while True:
-    mentions = api.mentions_timeline(since_id=mention_id) # trouver les tweets ( question) qu'on recherche
-    # iteration a chaque tweet trouvé (qu'on recherche)
+    mentions = api.mentions_timeline(since_id=mention_id) # trouver les tweets qui nous mentionne
+    # iteration a chaque tweet trouvé qui nous mentionne
     for mention in mentions:
         print("tweet trouvé")
-        print(f"{mention.author.screen_name} - {mention.text}") #imprimer le tweet trouvé
+        print(f"{mention.author.screen_name} - {mention.text}") #imprimer le tweet trouvé qui nous mentionne
         mention_id = mention.id
        
-      if mention.in_reply_to_status_id is None and mention.author.id != bot_id:    # Vérifier si le tweet trouvé n'est pas une réponse et nous ne sommes pas l'auteur
-            if True in [word in mention.text.lower() for word in words]:           # Vérifier que le tweet trouvé contient l'un des mots de notre liste de mots (words) afin que nous puissions déterminer si le tweet pourrait être une question.
+      if mention.in_reply_to_status_id is None and mention.author.id != bot_id:    # Vérifier si le tweet trouvé qui nous mentionne  n'est pas une réponse et nous ne sommes pas l'auteur de ce tweet (ce n'est pas le bot qui a envoyé le tweet
+            if True in [word in mention.text.lower() for word in words]:           # Vérifier que le tweet trouvé qui nous mentionne contient l'un des mots de notre liste de mots (words) afin que nous puissions déterminer si le tweet pourrait être une question.
                 try: 
-                    #maintenant c'est la réponse 
+                    #maintenant c'est la réponse au tweet qui nous mentionne
                     print("Attempting to reply...") 
                     api.update_status(message.format(mention.author.screen_name), in_reply_to_status_id=mention.id_str)
                     print("Successfully replied :)")
                 except Exception as exc:
                     print(exc) 
-    time.sleep(15) #Le bot ne vérifiera les tweet que toutes les 15 secondes, sauf si vous modifiez cette valeur
+    time.sleep(15) #Le bot ne vérifiera les tweets qui nous mentionne  toutes les 15 secondes, sauf si on modifie la valeur
